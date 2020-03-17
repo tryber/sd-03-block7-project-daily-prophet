6d4a7405ff850b0d2b2d5826f8793d7202ed0ad4
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-17 18:49:18 -0300

    article cresce ao clicar

---
+++ script.js
@@ -0,0 +1,4 @@
+const article = document.getElementById('main-article');
+article.addEventListener('click', () => {
+  document.getElementById('main-article').classList.add('article-animation');
+});
