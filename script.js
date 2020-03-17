8464a7db124f2555f27b0566ed448400a3f81fab
rogerfaustino <roger_faustino@hotmail.com>
2020-03-17 15:11:17 -0300

    Starting the daily prophet project

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-Testing
+let f = document.getElementById('foo');
+document.addEventListener('click', function(ev){
+f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
+f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
+},false);
