ce55b0fe799f4e52d7432350dc582282c28a7979
rogerfaustino <roger_faustino@hotmail.com>
2020-03-17 17:41:18 -0300

    Codeclimate issues

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   const f = document.getElementById('article-animation');
-  document.addEventListener('click', function (ev) {
-  f.style.transform = 'translateY('+(ev.clientY + 10) + 'px)';
-  f.style.transform = 'translateX('+(ev.clientX + 10) + 'px)';
+  document.addEventListener('click', function () {
+    f.style.transform = 'article-grow';
 }, false);
 };
