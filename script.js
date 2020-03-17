14ae51fe0136260cf0ed7bac7606166554d26212
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-17 19:57:45 -0300

    css troll20222

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.onload = () => {
   const artigo = document.getElementsByTagName('article')[0];
-  artigo.addEventListener('click', () => artigo.classList.contains('article-animation') ? artigo.classList.remove('article-animation') : artigo.classList.add('article-animation'));
+  artigo.addEventListener('click', () => artigo.classList.add('article-animation'));
 };
