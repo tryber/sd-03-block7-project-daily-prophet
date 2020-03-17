8555ef61ae339cf62686f241091f6b1f1e8bfca9
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-17 19:32:30 -0300

    transform

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ article.addEventListener('click', () => {
   document.getElementById('main-article').classList.add('article-animation');
 });
 
-const header_title = document.getElementById('header-title');
-header_title.addEventListener('dblclick', () => {
-  header_title.classList.add('transfo');
+const headertitle = document.getElementById('header-title');
+headertitle.addEventListener('dblclick', () => {
+  headertitle.classList.add('transfo');
 });
