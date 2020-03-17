42b2f90ec3bd44a6d5dcb933d18623efe0ec1355
Jafet Henrique <jafetguerra6@gmail.com>
2020-03-17 18:45:40 -0300

    ajustando codeclimate

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
-  let article1 = document.querySelectorAll('.article-animation')[0];
-  console.log(article1);
+  const article1 = document.querySelectorAll('.article-animation')[0];
 
   article1.addEventListener('click', function () {
     article1.style.animationName = 'article-grow';
@@ -9,4 +8,4 @@ window.onload = function () {
   article1.addEventListener('mouseout', function () {
     article1.style.animationName = '';
   });
-}
+};
