5114b60c9b388eef4c3637fd7ab92a347d7389f2
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-17 20:31:34 -0300

    codeclimate

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const LEFT = document.getElementsByClassName('left-content')[0];
 const RIGHT = document.getElementsByClassName('right-content')[0];
 const SIDE = document.getElementsByClassName('side-bar')[0];
 
-MAIN.addEventListener('click',function () {
+MAIN.addEventListener('click', function () {
   MAIN.className = 'article-animation';
 });
 
-LEFT.addEventListener('click',function () {
+LEFT.addEventListener('click', function () {
   LEFT.className = 'article-animation';
 });
 
-RIGHT.addEventListener('click',function () {
+RIGHT.addEventListener('click', function () {
   RIGHT.className = 'article-animation';
 });
 
-SIDE.addEventListener('click',function () {
+SIDE.addEventListener('click', function () {
   SIDE.className = 'article-animation';
 });
