05268dc51d59150fed817a80df47259d59ae12fc
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-18 09:45:27 -0300

    Articles

--- script.js
+++
@@ -1,4 +0,0 @@
-const article = document.getElementById('main-article');
-article.addEventListener('click', () => {
-  document.getElementById('main-article').classList.add('article-animation');
-});
