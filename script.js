22ac4b7f1838ab13205394fb0150849fb7baed22
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-18 09:05:13 -0300

    animations and background

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ const article = document.getElementById('main-article');
 article.addEventListener('click', () => {
   document.getElementById('main-article').classList.add('article-animation');
 });
-
-const headertitle = document.getElementById('header-title');
-headertitle.addEventListener('dblclick', () => {
-  headertitle.classList.add('transfo');
-});
