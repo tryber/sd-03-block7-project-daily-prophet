e09cef6fe891b68c3ed76f8ae0f4f548ea4ceb1d
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-18 21:43:25 -0300

    Arrumando requisito 10

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// To do
+document.querySelectorAll('article').forEach((e) => {e.addEventListener('click', (event) => {
+  if(event.target.parentNode.tagName === 'ARTICLE'){
+    event.target.parentNode.classList.add('article-animation')
+  }
+})})
