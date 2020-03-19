07f5e4495be1f03bb030d4017fdc9ac9caa999a2
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-18 21:48:13 -0300

    Code Climate

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-document.querySelectorAll('article').forEach((e) => {e.addEventListener('click', (event) => {
-  if(event.target.parentNode.tagName === 'ARTICLE'){
-    event.target.parentNode.classList.add('article-animation')
-  }
-})})
+document.querySelectorAll("article").forEach( (e) => {
+  e.addEventListener("click", event => {
+    if (event.target.parentNode.tagName === "ARTICLE") {
+      event.target.parentNode.classList.add("article-animation");
+    }
+  });
+});
