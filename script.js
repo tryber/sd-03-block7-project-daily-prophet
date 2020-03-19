52d6646cf5390de416b3b797be6e973cf804219a
fernando_luckesi <fernando.luckesi94@gmail.com>
2020-03-19 19:34:14 -0300

    CC versão 2.2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function aumentarArtigos() {
   const articleAnimation = document.querySelectorAll('.article-animation');
   for (let i = 0; i < articleAnimation.length; i += 1) {
     articleAnimation[i].addEventListener('click', () => {
-      articleAnimation[i].style.animationPlayState = 'running'
+      articleAnimation[i].style.animationPlayState = 'running';
     });
   }
 }
