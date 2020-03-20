cdf97645013125b3741dc422a0a57d786c39e3f3
davidepadua <davidepadua1914@gmail.com>
2020-03-20 16:57:37 -0300

    Requisitos artigo

---
+++ script.js
@@ -0,0 +1,7 @@
+const clicado = document.querySelector(".mapa");
+clicado.addEventListener('click', _ =>
+clicado.classList.toggle('mapa-onclick'));
+
+const artigo = document.querySelector(".pArtigo");
+artigo.addEventListener('click', _ =>
+artigo.classList.toggle('article-animation'));
