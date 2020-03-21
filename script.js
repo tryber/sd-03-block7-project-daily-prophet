3045d0e370b6bcce23ad163489fcadd13bfa702e
wenderson belko <contato@wendersonbelko.com>
2020-03-21 15:09:17 -0300

    acabei com os bugs?

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
- 
+
+
 window.onload = function () {
-    const f = document.getElementById('article-animation');
-    document.addEventListener('click', function () {
+  const f = document.getElementById('article-animation');
+  document.addEventListener('click', function () {
       f.style.transform = 'article-grow';
     }, false);
-  };
+};
