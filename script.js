8af5d404bcd7e01e3c42b32bf60917377e115570
wenderson belko <contato@wendersonbelko.com>
2020-03-21 15:00:08 -0300

    vamos ver os bugs

---
+++ script.js
@@ -0,0 +1,7 @@
+ 
+window.onload = function () {
+    const f = document.getElementById('article-animation');
+    document.addEventListener('click', function () {
+      f.style.transform = 'article-grow';
+    }, false);
+  };
