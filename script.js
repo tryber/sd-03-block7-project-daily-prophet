0105d1e55dc2bcefef788f5b560cf23a923f8bac
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-22 17:15:06 -0300

    Adjusting requirements of animations, transitions and transforms

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function grow() {
-  document.getElementById('article').classList.toggle('article-animation');
+  document.getElementsByClassName('article-animation')[0].style.animationDuration = '4s';
+  document.getElementsByClassName('article-animation')[0].style.animationName = 'article-grow';
+  document.getElementsByClassName('article-animation')[0].style.animationTimingFunction = 'linear';
 }
 
 document.getElementById('article').addEventListener('click', grow);
