5ce2d2ccc202860e1d7ecebb82fc3242289116fc
Ga Pimentel <cadgap@gmail.com>
2020-03-22 16:04:23 -0300

    Ajustes no javascript

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 const artAlvo = document.querySelector('.article-animation');
 artAlvo.addEventListener('click', () => {
-  if (artAlvo.style.animationName === 'article-grow') {
-    artAlvo.style.animationName = 'unset';
-  } else {
-    artAlvo.style.animationName = 'article-grow';
-  }
+  artAlvo.style.animationPlayState = 'running';
 });
