d1d81057051431a5c43366b190c98165044b5521
Ga Pimentel <cadgap@gmail.com>
2020-03-22 15:39:31 -0300

    Ajustes Climate

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const artAlvo = document.querySelector('.article-animation');
 artAlvo.addEventListener('click', () => {
   if (artAlvo.style.animationName === 'article-grow') {
     artAlvo.style.animationName = 'unset';
-  }
-  else {
-    artAlvo.style.animationName = 'article-grow'
+  } else {
+    artAlvo.style.animationName = 'article-grow';
   }
 });
