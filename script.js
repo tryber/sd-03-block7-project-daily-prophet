cb08054b7a0b2af65bf174efeb5a2682746ff80b
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-22 16:56:44 -0300

    Ajusting CodeClimate

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function grow () {
-  document.getElementsByClassName('article-animation')[0].style.animationName = 'article-grow';
-  document.getElementsByClassName('article-animation')[0].style.animationDuration = '4s';
-  document.getElementsByClassName('article-animation')[0].style.animationTimingFunction = 'linear';
+function grow() {
+  document.getElementById('article').classList.toggle ('article-animation');
 }
 
 document.getElementById('article').addEventListener('click', grow);
