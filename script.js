efeaa12864e839556c94a24eed666f27962a8e8b
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-22 00:50:12 -0300

    code climate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
-    const artigos = document.querySelectorAll('.article-animation');
-    for (let i = 0; i < artigos.length; i += 1) {
-        let artigo = document.querySelectorAll('.article-animation')[i];
-        artigo.addEventListener('click', () => artigo.style.animationPlayState = 'running');
-    }
-}
+  const artigos = document.querySelectorAll('.article-animation');
+  for (let i = 0; i < artigos.length; i += 1) {
+    const artigo = document.querySelectorAll('.article-animation')[i];
+    artigo.addEventListener('click', () => artigo.style.animationPlayState = 'running');
+  }
+};
