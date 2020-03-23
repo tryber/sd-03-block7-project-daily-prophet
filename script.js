c84d91b2a8c5c377eeeb2ea0a1e11306e9b9950c
Marylange <marylangess@gmail.com>
2020-03-22 22:08:44 -0300

    requisito 11 teste

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const artigos = document.querySelectorAll('.article-animation');
 function animarArtigos() {
-  console.log(this);
   this.style.animationPlayState = 'running';
 }
 for (let i = 0; i < artigos.length; i += 1) {
