84edf6127c46810ebd4876e0562fd9a8ca997dfc
henrique <hbaetaleite@gmail.com>
2020-03-23 14:50:02 -0300

    Mais algumas correcoes para Requisitos

--- script.js
+++
@@ -1,5 +0,0 @@
-const cresceArtigo = document.querySelectorAll('article-animation');
-
-const addClasse = () => cresceArtigo.classList.add('animacao');
-
-cresceArtigo.addEventListener('click', addClasse);
