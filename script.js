07716a101f52e12263d1a0e7a3af6a8510f837a4
Marylange <marylangess@gmail.com>
2020-03-22 22:03:06 -0300

    requisito 11

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const artigos = document.querySelectorAll('.article-animation');
 function animarArtigos() {
+  console.log(this);
   this.style.animationPlayState = 'running';
 }
 for (let i = 0; i < artigos.length; i += 1) {
