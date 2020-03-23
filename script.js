86c61ef5d7415b6f52b292d20ebece570343663b
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-23 10:33:10 -0300

    Trying to attend requests

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function grow() {
-  document.getElementsByClassName('article-animation')[0].style.animationDuration = '4s';
-  document.getElementsByClassName('article-animation')[0].style.animationName = 'article-grow';
-  document.getElementsByClassName('article-animation')[0].style.animationTimingFunction = 'linear';
-}
 
-document.getElementById('article').addEventListener('click', grow);
