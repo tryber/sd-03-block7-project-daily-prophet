4ad284cf6d55d8cc25262e7d4904af05efdf062c
Marylange <marylangess@gmail.com>
2020-03-22 21:19:58 -0300

    teste

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const artigos = document.querySelectorAll('.article-animation');
 function animarArtigos() {
   this.style.animationPlayState = 'running';
 }
-
 for (let i = 0; i < artigos.length; i += 1) {
   artigos[i].addEventListener('click', animarArtigos);
-} ;
+} 
