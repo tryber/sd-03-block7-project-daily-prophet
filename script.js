221e20c0ec688c916d8082dffd862f94d1785866
wscatao <wscatao@gmail.com>
2020-03-23 13:06:50 -0300

    codeclimate fix

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const artigos = document.querySelectorAll('#article');
-console.log(artigos);
 
 for (let i = 0; i < artigos.length; i += 1) {
   artigos[i].addEventListener('click', (ev) => {
-    let teste = ev.currentTarget
+    const teste = ev.currentTarget;
     if (teste.classList.contains('article-animation')) {
       teste.classList.remove('article-animation');
     } else {
