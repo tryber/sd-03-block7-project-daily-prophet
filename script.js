f5f1bb871fc3cad6d246c3a6f864aa87ab2415e3
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-24 00:11:47 -0300

    codeclimate

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ window.onload = () => {
   const articles = document.querySelectorAll('article');
   articles.forEach((elem) => {
     elem.addEventListener('click', () => {
-      let state = window.getComputedStyle(elem).animationPlayState;
+      const state = window.getComputedStyle(elem).animationPlayState;
       if (state === 'running') {
         elem.style.animationPlayState = 'paused';
-      }
-      else if (state === 'paused') {
+      } else if (state === 'paused') {
         elem.style.animationPlayState = 'running';
       }
     });
     elem.addEventListener('click', () => {
       elem.addEventListener('click', () => {
         elem.classList.toggle('article-animation');
-      })
+      });
     });
   });
 };
