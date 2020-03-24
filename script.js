e0e9726f853b242227f3315d329cccf88e849a7a
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-23 22:44:45 -0300

    level up on css quality and creativety

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  const articles = document.querySelectorAll('article');
+  articles.forEach( (elem) => {
+    elem.addEventListener('mouseover', () => {
+      elem.classList.add('article-animation');
+    });
+    elem.addEventListener('mouseout', () => {
+      elem.classList.remove('article-animation');
+    });
+  });
+}
