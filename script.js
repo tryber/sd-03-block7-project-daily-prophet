672eb87095682c2e3cf7655fbb6e1cf8aefaa6e9
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-24 00:06:00 -0300

    mudanca no script para manter o css como pedido

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ window.onload = () => {
   const articles = document.querySelectorAll('article');
   articles.forEach((elem) => {
     elem.addEventListener('click', () => {
-      elem.classList.toggle('article-animation');
+      let state = window.getComputedStyle(elem).animationPlayState;
+      if (state === 'running') {
+        elem.style.animationPlayState = 'paused';
+      }
+      else if (state === 'paused') {
+        elem.style.animationPlayState = 'running';
+      }
+    });
+    elem.addEventListener('click', () => {
+      elem.addEventListener('click', () => {
+        elem.classList.toggle('article-animation');
+      })
     });
   });
 };
