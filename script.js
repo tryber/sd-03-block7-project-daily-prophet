76efb028c711426265b505f6495e34f7cb7bbf38
rogerfaustino <roger_faustino@hotmail.com>
2020-03-24 10:28:54 -0300

    Some adjustments

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ window.onload = function () {
   const f = document.getElementById('article-animation');
   document.addEventListener('click', function () {
     f.style.transform = 'article-grow';
-  }, false);
+  });
 };
