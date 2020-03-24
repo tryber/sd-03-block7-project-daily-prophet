273688f2bad67c1ee531e75b5e46c8b50f9f03a5
mirellaalves <mirella.alves@gmail.com>
2020-03-24 01:08:37 -0300

    Codeclimate adjustments.

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const article = document.querySelector('.article-animation');
 const start = document.querySelector('article');
 
 function startAnimations() {
-    article.style.animationName = 'article-grow';
-    article.style.animationDuration = '4s';
-    article.style.animationTimingFunction = 'linear';
-    article.style.animationPlayState = 'running';
+  article.style.animationName = 'article-grow';
+  article.style.animationDuration = '4s';
+  article.style.animationTimingFunction = 'linear';
+  article.style.animationPlayState = 'running';
 }
 
-start.addEventListener('click', function() {
-    startAnimations();
-})
+start.addEventListener('click', function () {
+  startAnimations();
+});
