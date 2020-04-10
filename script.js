b4a1c44a8480178ae71f506294bfec8aa758aab0
leonardodamasceno8 <leonardodamasceno8@users>
2020-04-09 23:41:09 -0300

    code climate hell

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const artOne = document.getElementById('art-1');
 const artTwo = document.getElementById('art-2');
 const artTree = document.getElementById('art-3');
 
-const one = () => artOne.style.animationPlayState = 'running';
+const one = () => {
+  artOne.style.animationPlayState = 'running';
+};
 
-const two = () => artTwo.style.animationPlayState = 'running';
+const two = () => {
+  artTwo.style.animationPlayState = 'running';
+};
 
-const tree = () => artTree.style.animationPlayState = 'running';
+const tree = () => {
+  artTree.style.animationPlayState = 'running';
+};
 
 window.onload = function () {
-	artOne.addEventListener('click', one);
-	artTwo.addEventListener('click', two);
-	artTree.addEventListener('click', tree);
+  artOne.addEventListener('click', one);
+  artTwo.addEventListener('click', two);
+  artTree.addEventListener('click', tree);
 };
